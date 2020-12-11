769be190d0043b84d6bc1bd142feaf0a6250f7fc
DenisKamchatov <71318867+DenisKamchatov@users.noreply.github.com>
2020-12-12 01:03:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// 1 Переменные
+
+var name = 'Denis' // Устаревшая
+const sec = 'Kamchatov' 
+let age = 19
+console.log(age) // Вывести в консоль в браузере
+
+// 2 Мутирование
+// console.log('Имя человека: ' + name + ', а возраст: ' + age) // JS автоматически преобразует age в string
+
+// const firstName = prompt('Введите ваше имя: ')
+// alert('Привет, ' + firstName)
+
+// 3 Операторы
+// console.log(++age) // ++ - оператор увеличения на 1, аналогично --
