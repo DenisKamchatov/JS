140396dea5575a3bb06b5f26fcf9708849d145eb
Denis Kamchatov <kamcatovdenis@gmail.com>
2020-12-14 00:15:13 +0500

    Create 03_string.js

---
+++ 03_string.js
@@ -0,0 +1,31 @@
+// const name = 'Денис'
+// const age = '19'
+
+// const output = `Привет, меня зовут ${name} и мне ${age} лет` // Более удобный способ записи вывода
+// console.log(output)      
+
+// const name = 'Денис'
+// console.log(name.toUpperCase()) // Все буквы большИе
+// console.log(name.toLowerCase())
+// console.log(name.charAt(2)) // Выводит символ под 2 индексом  
+// console.log(name.indexOf('ни')) // Проверяет есть ли такие символы в переменной 
+// console.log(name.startsWith('Ден')) // Сравнивает и проверяет сходятся ли  символы с началом переменной
+// console.log(name.endsWith('ис')) // Сравнивает и проверяет сходятся ли символы с концом переменной
+// console.log(name.repeat(3)) // Выводит переменную в строчку 3 раза 
+
+// const string = '    password    '
+// console.log(string.trim())
+// console.log(string.trimLeft()) // Очищает пробелы в переменной
+// console.log(string.trimRight())  
+
+function logPerson(s, name, age) {
+    console.log(s, name, age)
+    return `${s[0]}${name}${s[1]}${age}`
+}
+
+personName = 'Денис'
+personAge = 19
+
+const output = logPerson`Имя: ${personName}, Возраст: ${personAge}`
+
+console.log(output)
